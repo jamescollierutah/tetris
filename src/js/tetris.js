84de3a3a88c944775695a8f9270faa0fb0f64740
James Collier <jcollier@axis41.com>
2016-04-06 14:18:21 -0600

    trying to find canvas

--- src/js/tetris.js
+++ src/js/tetris.js
@@ -3,7 +3,8 @@
  */
 
 var renderer = PIXI.autoDetectRenderer(800, 600,{backgroundColor : 0x1099bb});
-document.body.appendChild(renderer.view);
+document.getElementById("tetris-canvas").appendChild(renderer.view);
+//document.body.appendChild(renderer.view);
 
 // create the root of the scene graph
 var stage = new PIXI.Container();
