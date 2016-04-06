af7d06a05b3aa9b27947a5b5e312c61517c01589
James Collier <jcollier@axis41.com>
2016-04-06 14:21:50 -0600

    wrapping pixi stuff in jquery on ready

--- src/js/tetris.js
+++ src/js/tetris.js
@@ -2,42 +2,44 @@
  * Created by jcollier on 4/6/16.
  */
 
-var renderer = PIXI.autoDetectRenderer(800, 600,{backgroundColor : 0x1099bb});
-document.getElementById("tetris-canvas").appendChild(renderer.view);
+$(function() {
+    var renderer = PIXI.autoDetectRenderer(800, 600,{backgroundColor : 0x1099bb});
+    document.getElementById("tetris-canvas").appendChild(renderer.view);
 //document.body.appendChild(renderer.view);
 
 // create the root of the scene graph
-var stage = new PIXI.Container();
+    var stage = new PIXI.Container();
 
-var container = new PIXI.Container();
+    var container = new PIXI.Container();
 
-stage.addChild(container);
+    stage.addChild(container);
 
-for (var j = 0; j < 5; j++) {
+    for (var j = 0; j < 5; j++) {
 
-    for (var i = 0; i < 5; i++) {
-        var bunny = PIXI.Sprite.fromImage('_assets/basics/bunny.png');
-        bunny.x = 40 * i;
-        bunny.y = 40 * j;
-        container.addChild(bunny);
+        for (var i = 0; i < 5; i++) {
+            var bunny = PIXI.Sprite.fromImage('_assets/basics/bunny.png');
+            bunny.x = 40 * i;
+            bunny.y = 40 * j;
+            container.addChild(bunny);
+        };
     };
-};
-/*
- * All the bunnies are added to the container with the addChild method
- * when you do this, all the bunnies become children of the container, and when a container moves,
- * so do all its children.
- * This gives you a lot of flexibility and makes it easier to position elements on the screen
- */
-container.x = 100;
-container.y = 60;
+    /*
+     * All the bunnies are added to the container with the addChild method
+     * when you do this, all the bunnies become children of the container, and when a container moves,
+     * so do all its children.
+     * This gives you a lot of flexibility and makes it easier to position elements on the screen
+     */
+    container.x = 100;
+    container.y = 60;
 
 // start animating
-animate();
+    animate();
 
-function animate() {
+    function animate() {
 
-    requestAnimationFrame(animate);
+        requestAnimationFrame(animate);
 
-    // render the root container
-    renderer.render(stage);
-}
+        // render the root container
+        renderer.render(stage);
+    }
+});
